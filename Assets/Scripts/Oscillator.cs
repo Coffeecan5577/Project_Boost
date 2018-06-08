ac98ef59937be7334f1a011f07a8e7fb8633f285
Cale Curry <c.curry2210@gmail.com>
2018-06-08 11:55:09 -0500

    Created an oscillator script for moving platforms.

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] // Attribute
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementVector;
+
+    // TODO remove from inspector later.
+    [Range(0, 1)]
+    [SerializeField] //Attribute
+    private float _movementFactor; // 0 for not moved, 1 for fully moved.
+
+    private Vector3 _startingPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 offset = _movementFactor * _movementVector;
+	    transform.position = _startingPosition + offset;
+	}
+}
